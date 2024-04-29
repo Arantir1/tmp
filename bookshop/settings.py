139df22e175b1a716532bb6985b306ffac460c0e
Eugene Kobyak <evgenkb1@gmail.com>
2024-04-29 20:50:09 +0200

    Update settings.py

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "bookshop.wsgi.application"
 
 DATABASES = {
     'default': {
-        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "ENGINE": 'django.db.backends.sqlite3',
         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
